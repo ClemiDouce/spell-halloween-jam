91638cb6941a96e02e6a630b74d91caae5ad6043
Clementine Ducournau <dupycournau@gmail.com>
2025-11-04 10:38:28 +0100

    correct some things

--- content/player/interaction_area.gd
+++ content/player/interaction_area.gd
@@ -1,7 +1,7 @@
 class_name InteractionDetector extends Area2D
 
 @export var interaction_label : Label
-var current_interaction : Interactable = null
+var current_interaction : Array[Interactable]
 var can_interact := true
 
 func _ready() -> void:
@@ -10,26 +10,38 @@ func _ready() -> void:
 
 func _input(event: InputEvent):
 	if event.is_action_pressed("interact") and can_interact:
-		if current_interaction and current_interaction.is_interactable:
-			can_interact = false
-			#interaction_label.hide()
-			current_interaction.hide_prompt()
-			await current_interaction.interaction.call(owner)
-			#interaction_label.show()
-			if current_interaction:
-				current_interaction.show_prompt()
-			can_interact = true
+		if current_interaction and can_interact:
+			for interaction in current_interaction:
+				if interaction.is_interactable:
+					on_interaction(interaction)
+					return
+				else:
+					continue
+
+func _process(_delta: float) -> void:
+	if current_interaction and can_interact:
+		current_interaction.sort_custom(_sort_by_nearest)
+		for i in current_interaction.size():
+			if i == 0:
+				current_interaction[i].show_prompt()
+			else:
+				current_interaction[i].hide_prompt()
 
 func on_interactable_entered(area: Area2D):
-	if not (area as Interactable).is_interactable:
-		return
-	current_interaction = area as Interactable
-	interaction_label.text = current_interaction.interact_name
-	current_interaction.show_prompt()
+	current_interaction.push_back(area as Interactable)
 	
-func on_interactable_exited(_area: Area2D):
-	if current_interaction == null:
-		return
-	current_interaction.hide_prompt()
-	current_interaction = null
+func on_interactable_exited(area: Area2D):
+	area.hide_prompt()
+	current_interaction.erase(area)
 	
+func _sort_by_nearest(area1, area2):
+	var area1_dist = global_position.distance_to(area1.global_position)
+	var area2_dist = global_position.distance_to(area2.global_position)
+	return area1_dist < area2_dist
+	
+func on_interaction(area: Interactable):
+	can_interact = false
+	area.hide_prompt()
+	await area.interaction.call(owner)
+	area.show_prompt()
+	can_interact = true
