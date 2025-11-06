458d029fad498bac1d529d16f0c548546c7b8dc7
Clementine Ducournau <dupycournau@gmail.com>
2025-11-06 11:48:31 +0100

    add musics

--- scenes/main_scene.gd
+++ scenes/main_scene.gd
@@ -1,3 +1,12 @@
 extends Node2D
 
-@export var fire_camp : Interactable
+@onready var music : AudioStreamPlaybackInteractive = %MusicPlayer.get_stream_playback()
+var current_zone := "Starting Zone"
+
+func _ready() -> void:
+	Events.player_entered_zone.connect(on_player_changing_zone)
+		
+func on_player_changing_zone(zone: Zone):
+	if zone.zone_name == "zombie_project_final_v56":
+		current_zone = zone.zone_name
+		music.switch_to_clip_by_name("crow")
